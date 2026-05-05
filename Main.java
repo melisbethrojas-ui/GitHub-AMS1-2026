353ddae6d23e8c2d5d1761ec851128af2a145445
Anna121104 <119697202+AnnSan00@users.noreply.github.com>
2026-05-05 18:05:20 +0200

    Cambiando línea 4 para conflicto.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proyecto grupal)");
+        System.out.println("Versión de P3 para conflicto");
         System.out.println("Anna Puig (Reviewer)");
 
     }
