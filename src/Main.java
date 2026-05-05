03278ecf002c974feaec750015fda03b5486a328
melisbethrojas-ui <melisbethrojas@gmail.com>
2026-05-05 18:15:53 +0200

    actualizacion del Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proyecto Java para practicar GitHub y ramas.");    }
+        System.out.println("Proyecto Java para practicar GitHub y ramas.");    
+        System.out.println("Linea de conflicto");  
+    }
 }
