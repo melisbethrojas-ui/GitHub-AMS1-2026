dbbffdfed346c3933ab9cd85285405b2fcd82c7e
Anna121104 <119697202+AnnSan00@users.noreply.github.com>
2026-05-05 16:51:42 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto grupal)");
-        System.out.println("Proyecto grupal - Anna Puig (Reviewer)");
+        System.out.println("Anna Puig (Reviewer)");
 
     }
 }
